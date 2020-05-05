c3510ba2957072b1c1f0741e2d15360e45310114
onifademola <onifademola@gmail.com>
2020-05-05 13:46:21 +0100

    fix: linting issue fix v4

--- proj_backend/src/db/index.js
+++ proj_backend/src/db/index.js
@@ -1,18 +1,14 @@
 const { Pool } = require('pg');
-const { dbDatabase, dbHost, 
-  dbPassword, dbPort, dbUser
-} = require('../../config');
+const { dbDatabase, dbHost, dbPassword, dbPort, dbUser } = require('../../config');
 
 const pool = new Pool({
   user: dbUser,
   host: dbHost,
   database: dbDatabase,
   password: dbPassword,
-  port: dbPort,
+  port: dbPort
 });
 
 module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback);
-  }
+  query: (text, params, callback) => { return pool.query(text, params, callback); }
 };
